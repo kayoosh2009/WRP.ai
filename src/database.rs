bbfc5532a4e725f6c35043b8bf886c8a2afe5597
kayoosh2009 <kayoosh2009@gmail.com>
2026-09-12 14:47:23 +0300

    1.0.8 (156-e)

--- src/database.rs
+++ src/database.rs
@@ -986,33 +986,7 @@ impl FirestoreDb {
         Ok(())
     }
 
-    /// Общая статистика сайта: сколько всего персонажей, аккаунтов и отправленных сообщений
-    pub async fn get_site_stats(&self) -> Result<crate::model::SiteStats, Box<dyn std::error::Error>> {
-        let characters = self.get_all_characters().await.unwrap_or_default();
-        let characters_created = characters.len() as u64;
-
-        let url = format!("{}/users?key={}", self.base_url(), self.api_key);
-        let response = self.client.get(&url).send().await?;
-
-        let (accounts_created, messages_sent) = if response.status().is_success() {
-            let list_response: FirestoreListResponse = response.json().await?;
-            let accounts = list_response.documents.len() as u64;
-            let messages: u64 = list_response
-                .documents
-                .iter()
-                .map(|doc| get_integer_field(&doc.fields, "messages_sent").unwrap_or(0) as u64)
-                .sum();
-            (accounts, messages)
-        } else {
-            (0, 0)
-        };
-
-        Ok(crate::model::SiteStats {
-            characters_created,
-            accounts_created,
-            messages_sent,
-        })
-    }
+    pub async fn get_token_usage_stats(&self, id_token: &str) -> Result<Vec<TokenUsageStat>, Box<dyn std::error::Error>> {
         let url = format!("{}/token_usage?key={}", self.base_url(), self.api_key);
 
         let response = self.client
